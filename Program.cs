9b1d5759de1e08b64906f935683a8a83b3f6b17f
ViniHGV <ixxvinicius@gmail.com>
2023-12-21 17:00:30 -0300

    feat: criando objeto para cada classe juntamentamente a seus parâmetros

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using ParkModel;
+using MenuModel;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Park park = new Park(new List<string>(), 1, 1);
+        Menu menu = new Menu("4");
+
+        menu.ListMenu(park);
+    }
+}
