7876c8e0dd42082462eff4e4b56c2d016a3bb67f
ViniHGV <ixxvinicius@gmail.com>
2023-12-26 18:47:02 -0300

    feat: criando a interface IPark para a implementação  e validação da classe park

---
+++ interfaces/IPark.cs
@@ -0,0 +1,9 @@
+namespace Interfaces.IPark
+{
+    public interface IPark
+    {
+        void AddCar(string Car);
+        void ListCars();
+        void RemoveCar (string Car, int QuantityHours);
+    }
+}
