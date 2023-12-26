f10cd633e778dd795283b0c17c4c54f55f443f09
ViniHGV <ixxvinicius@gmail.com>
2023-12-26 13:48:12 -0300

    feat: editando método construtor e tipo de propiedades como priceinital e perhour, adicionando validações e exibindo o valor decimal com a conversão de moeda para real

--- model/Park.cs
+++ model/Park.cs
@@ -2,15 +2,15 @@ namespace ParkModel
 {
     public class Park
     {
-        public Park(List<string> Cars, int PriceInitial, int PricePerHour)
+        public Park()
         {
-            this.Cars = Cars;
-            this.PriceInitial = PriceInitial;
-            this.PricePerHour = PricePerHour;
+            Cars = new List<string>();
+            PriceInitial = 0.1M;
+            PricePerHour = 0.1M;
         }
         private List<string> _cars;
-        private int _priceInitial;
-        private int _pricePerHour;
+        private decimal _priceInitial;
+        private decimal _pricePerHour;
 
         public List<string> Cars { 
             get => _cars; 
@@ -21,7 +21,7 @@ namespace ParkModel
                 _cars = value;
             }
         }
-        public int PriceInitial { 
+        public decimal PriceInitial { 
             get => _priceInitial;
             set{
                 if(value <= 0){
@@ -30,7 +30,7 @@ namespace ParkModel
                 _priceInitial = value;
             } 
         }
-        public int PricePerHour { 
+        public decimal PricePerHour { 
             get => _pricePerHour;
             set{
                 if(value <= 0){
@@ -49,20 +49,22 @@ namespace ParkModel
         }
 
         public void ListCars(){
+            if(Cars.Count >0){
             Console.WriteLine("Listagem de carros no Estacionamento:");
             foreach(string Car in Cars){
                 Console.WriteLine(Car);
+            }}else{
+                Console.WriteLine("Nenhum carro cadastrado até o momento!!");
             }
         }
 
-        public void RemoveCar(string Car, int QuantityHours){
-            int TotalPrice = (QuantityHours * PricePerHour) + PriceInitial;
-
+        public void RemoveCar (string Car, int QuantityHours){
             if(Cars.Contains(Car)){
                 Cars.Remove(Car);
-                Console.WriteLine($"Carro com a placa {Car} removido com sucesso Total da permanência: {TotalPrice}");
+                decimal TotalPrice = (QuantityHours * PricePerHour) + PriceInitial;
+                Console.WriteLine($"\nCarro com a placa {Car} removido com sucesso Total da permanência: {TotalPrice.ToString("C")}");
             }else{
-                Console.WriteLine("Este carro não está no estacionamento! Confira a placa");
+                Console.WriteLine("\nEste carro não está no estacionamento! Confira a placa!!");
             }
 
         }
