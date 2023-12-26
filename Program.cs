d6497dc62ee58e98dd4163de2fd9d1511a3574db
ViniHGV <ixxvinicius@gmail.com>
2023-12-26 13:44:33 -0300

    refactor: refatorando passagem de parâmetros dos construtores pela Main

--- Program.cs
+++ Program.cs
@@ -5,8 +5,8 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Park park = new Park(new List<string>(), 1, 1);
-        Menu menu = new Menu("4");
+        Park park = new Park();
+        Menu menu = new Menu();
 
         menu.ListMenu(park);
     }
