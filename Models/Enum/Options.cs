323df319cf7d635b68216ac63809b9c846f66ce1
ViniHGV <ixxvinicius@gmail.com>
2023-12-27 17:53:28 -0300

    feat: criando Enum Options para verificação de opções do Menu

---
+++ Models/Enum/Options.cs
@@ -0,0 +1,11 @@
+namespace Parking.Models.Enum
+{
+    public enum Options
+    {
+        MenuAddCar = 1,
+        ListCars = 2,
+        ExitCar = 3,
+        ExitSystem = 4,
+        DisplayMenu = 5
+    }
+}
